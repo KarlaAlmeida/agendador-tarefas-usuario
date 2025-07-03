bf53e60a8f8975d19c196525f4f1167bb2c3773a
KarlaAlmeida <karlakcbam@gmail.com>
2025-07-02 21:04:58 -0300

    commit inicial

---
+++ src/main/java/com/javanauta/agendador_tarefas_usuario/AgendadorTarefasUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.javanauta.agendador_tarefas_usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorTarefasUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorTarefasUsuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javanauta/agendador_tarefas_usuario/AgendadorTarefasUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanauta.agendador_tarefas_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefasUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
